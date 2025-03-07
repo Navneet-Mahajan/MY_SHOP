82ae372155c9a958417c289239c4927442b77c78
Navneet Mahajan <bsnavneet84@gmail.com>
2025-03-07 18:03:26 +0530

    Configuration of Multer

---
+++ Middleware/multer.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const randomString = require("../Util/enc").generateRandomString;
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, res, next) => {
+      next(null, "public/imagesUser");
+    },
+    filename: (req, file, next) => {
+      next(
+        null,
+        file.mimetype.split("/")[0].toLowerCase() +
+          "_" +
+          randomString() +
+          "_" +
+          Date.now() +
+          path.extname(file.originalname).toLowerCase()
+      );
+    },
+  }),
+  limits: { fileSize: 104857600 },
+});
+
+module.exports = upload;
