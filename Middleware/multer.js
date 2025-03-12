50445dea16157872293c6ffcc1670137070369f0
Navneet Mahajan <bsnavneet84@gmail.com>
2025-03-12 12:33:57 +0530

    changed address of destination

--- Middleware/multer.js
+++ Middleware/multer.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const upload = multer({
   storage: multer.diskStorage({
     destination: (req, res, next) => {
-      next(null, "public/imagesUser");
+      next(null, "public/images");
     },
     filename: (req, file, next) => {
       next(
