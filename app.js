3d5fdc3e16736cc75f4e677a2ae5d5f4564b0a86
Navneet Mahajan <bsnavneet84@gmail.com>
2025-03-12 12:33:18 +0530

    Added Passport and Validation Error

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 require("dotenv").config("./.env");
+require("./Middleware/passport");
 const express = require("express");
 const api = require("./routes/index.routes");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const db = require("./config/db");
+const passport = require("passport");
 app.use(express.json());
+app.use(express.static("public/"));
+app.use([passport.initialize()]);
+
+//validation of data
+const { ValidationError } = require("express-validation");
+const { path } = require("./models/userSchema");
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    const details = err.details.map((detail) => ({
+      message: detail.message,
+      path: detail.path,
+      type: detail.type,
+    }));
+
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message || "Validation-Error",
+      details: err.details,
+    });
+  }
+  next(err); // Pass other errors to the next middleware
+});
 
 // routes from index.routes.js
-
 app.use("/", api);
 app.get("/", (req, res) => {
   res.send("This is the landing page ");
 });
-
 //Listening the server
 
 app.listen(PORT, (req, res) => {
