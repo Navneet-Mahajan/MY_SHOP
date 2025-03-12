7beeeb263a94b9df5e00303213d9ff81f9d4d466
Navneet Mahajan <bsnavneet84@gmail.com>
2025-03-12 16:36:32 +0530

    adding comments

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+//importing env and Passport configurations
 require("dotenv").config("./.env");
 require("./Middleware/passport");
+
+//Initializing express app
 const express = require("express");
-const api = require("./routes/index.routes");
-const PORT = process.env.PORT || 3000;
 const app = express();
+
+//routes
+const api = require("./routes/index.routes");
 const db = require("./config/db");
+const PORT = process.env.PORT || 3000;
+
+//app.use
 const passport = require("passport");
 app.use(express.json());
 app.use(express.static("public/"));
