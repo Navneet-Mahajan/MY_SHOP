7ccac2b8136e5d66cde4e91631918aab0f5094c7
Navneet Mahajan <bsnavneet84@gmail.com>
2025-03-12 16:37:20 +0530

    Added Cart Routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+//Importing route files
 const users = require("./users.js");
 const products = require("./products.js");
+const cart = require("./cart.js");
 
+//using file on routes
 router.use("/api/users", users);
 router.use("/api/products", products);
-
+router.use("/api/cart", cart);
 module.exports = router;
